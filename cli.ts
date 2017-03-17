bc4af41f3ec9b39fa8c3ce639c589bfd2e7ef271
576625322@qq.com <576625322@qq.com>
2017-03-17 22:58:01 +0800

    fix: path error

--- cli.ts
+++ cli.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
 import { execSync } from 'child_process'
+import * as path from 'path'
 
-const child = execSync(`./node_modules/.bin/concurrently --kill-others --prefix command "node built/src/webpack-dev-server.js"`, {
+const concurrentlyPath = path.join(__dirname, '../node_modules/.bin/concurrently')
+const webpackDevServerPath = path.join(__dirname, 'src/webpack-dev-server.js')
+
+const child = execSync(`${concurrentlyPath} --kill-others --prefix command "node ${webpackDevServerPath}"`, {
     stdio: 'inherit'
 })
