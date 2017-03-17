4e90760cbdfff18092b89f013f66181bd6277e2c
576625322@qq.com <576625322@qq.com>
2017-03-17 22:50:22 +0800

    fix bug

--- cli.ts
+++ cli.ts
@@ -2,6 +2,6 @@
 
 import { execSync } from 'child_process'
 
-const child = execSync('npm start', {
+const child = execSync(`./node_modules/.bin/concurrently --kill-others --prefix command "node built/src/webpack-dev-server.js"`, {
     stdio: 'inherit'
 })
