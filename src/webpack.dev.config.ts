b02651e225df2ca918db01d210e57450d3fbd002
ascoders <576625322@qq.com>
2017-08-21 09:59:31 +0800

    change ts-loader

--- src/webpack.dev.config.ts
+++ src/webpack.dev.config.ts
@@ -29,7 +29,7 @@ const webpackConfig = {
             }, {
                 test: /\.(tsx|ts)?$/,
                 exclude: [/node_modules/],
-                use: ['react-hot-loader', 'awesome-typescript-loader']
+                use: ['react-hot-loader', 'ts-loader']
             }, {
                 test: /\.json$/,
                 use: ['json-loader']
